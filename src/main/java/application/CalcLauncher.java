981b544457c00afb18f3b9555292e6a95a02e89d
robjsacco <robjsacco+github@gmail.com>
2019-09-24 20:27:47 -0400

    Add FatJar to gradle to allow deployment

--- src/main/java/application/CalcLauncher.java
+++ src/main/java/application/CalcLauncher.java
@@ -6,18 +6,19 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-
 public class CalcLauncher extends Application {
   @Override
   public void start(Stage stage) throws Exception {
-    Parent root = FXMLLoader.load(getClass().getResource("/view/GuiAccordion.fxml"));
-    //Parent root = FXMLLoader.load(getClass().getResource("/view/GUI.fxml"));
-    
-    Scene scene = new Scene(root, 600, 470);
 
-    stage.setTitle("BudgetCalculator");
-    stage.setScene(scene);
-    stage.show();
+//    System.out.println("Current Locale: " + Locale.getDefault());
+//
+//    URL location = getClass().getResource("GuiAccordion.fxml");
+   //ResourceBundle resources = ResourceBundle.getBundle("fxml");
+ //   FXMLLoader fxmlLoader = new FXMLLoader(location, resources);
+  //  Parent root = FXMLLoader.load(location, resources);
+    Parent root = FXMLLoader.load(getClass().getResource("/fxml/GUI.fxml"));
+    //Parent root = FXMLLoader.load(getClass().getResource("/view/GUI.fxml"));
+   CalcGuiController.setPrimaryStage(stage, root);
   }
 
   public static void main(String[] args) {
